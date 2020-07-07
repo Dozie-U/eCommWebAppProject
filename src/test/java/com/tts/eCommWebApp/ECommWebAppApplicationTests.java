73df9f5e0378a76430cc788e1f76aab9e322b1ec
martin9719 <martinsalas_9715@yahoo.com>
2020-07-07 09:24:10 -0500

    first commit

---
+++ src/test/java/com/tts/eCommWebApp/ECommWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tts.eCommWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
